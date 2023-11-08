86cd6282ef32a5cc44527fad9052a9021dd465b9
Tobias Roeske <t.roeske91@gmail.com>
2023-11-08 22:31:41 +0100

    JS extended

--- script.js
+++ script.js
@@ -1,15 +1,55 @@
 const numArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const operators = ['plus', 'minus', 'multiply', 'divide'];
+const isEqual = document.getElementById('isEqual');
 const result = document.getElementById('result');
-const plus = document.getElementById('plus');
-const minus = document.getElementById('minus');
-const multiply = document.getElementById('multiply');
-const divide = document.getElementById('divide');
-function putToResult(arr) {
 
+let inBetweenResult = [];
+let newText = document.createElement('div');
+result.append(newText)
+
+const resultDisplay = result.querySelector('div').style.display;
+
+
+function putNumToResult(arr) {
     for (let i = 0; i < arr.length; i++) {
         document.getElementById(arr[i]).addEventListener('click', () => {
-            result.innerHTML += arr[i];
+            newText.innerHTML += arr[i];
         });
     }
 }
 
+function changeOperatorSymbol(origSymbol, newSymbol) {
+    document.getElementById(origSymbol).addEventListener('click', () => {
+        newText.innerHTML += newSymbol;
+    })
+}
+
+function putOperatorToResult(arr) {
+    for (let i =0; i < arr.length; i++) {
+        switch (arr[i]) {
+            case 'plus':
+                changeOperatorSymbol('plus', ' + ');
+                break;
+            case 'minus':
+                changeOperatorSymbol('minus', ' - ');
+                break;
+            case 'multiply':
+                changeOperatorSymbol('multiply', ' * ');
+                break;
+            case 'divide':
+                changeOperatorSymbol('divide', ' / ');
+                break;
+        }
+    }
+}
+
+
+function clear() {
+    document.getElementById('btn-ac').addEventListener('click', () => {
+        newText.innerHTML = '';
+    });
+}
+
+putNumToResult(numArr);
+putOperatorToResult(operators);
+clear();
